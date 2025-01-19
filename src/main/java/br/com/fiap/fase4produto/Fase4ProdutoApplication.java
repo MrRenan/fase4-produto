54cd1d1ec1ac9557f7d4203fab4d1572a34f2be6
Renan Leite <renan.carli@hotmail.com>
2025-01-19 20:50:38 -0300

    First Commit

---
+++ src/main/java/br/com/fiap/fase4produto/Fase4ProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase4ProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fase4ProdutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/fase4produto/Fase4ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase4ProdutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
